2c0234871ac57a6a2065cac7613ef91793706e1f
Olga <ol.kras.andr@gmail.com>
2019-02-07 16:56:52 +0300

    last-commit-of-first-lesson

--- src/test/java/ru/seleniumTest/BaseDriverClass.java
+++ src/test/java/ru/seleniumTest/BaseDriverClass.java
@@ -4,15 +4,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseDriverClass {
     private static WebDriver driver;
-    private String baseUrl = "http://localhost:8080/registration-form.html";
+    private static String baseUrl = "http://localhost:8080/registration-form.html";
 
-    @BeforeClass
+    @BeforeTest
     public static void setUp() {
 
         System.setProperty("webdriver.chrome.driver", "F:\\Project\\TestProject\\chromedriver.exe");
@@ -45,7 +47,7 @@ public class BaseDriverClass {
         driver.get(baseUrl);
     }
 
-    @AfterClass
+    @AfterTest
     public void shutDown(){
         driver.close();
     }
---
+++ src/test/java/ru/seleniumTest/CopyRegistration.java
@@ -0,0 +1,20 @@
+package ru.seleniumTest;
+
+import org.testng.annotations.Test;
+
+public class CopyRegistration extends BaseDriverClass {
+
+    @Test
+    public void getRegistrationFormWithValidValues(){
+        RegistrationFieldsObject validRegistrationFieldsObject = new RegistrationFieldsObject("test@test.test", "123456");
+        fillRegistrationFields(validRegistrationFieldsObject);
+    }
+
+    @Test
+    public void getRegistrationFormWithEmptyValues(){
+        RegistrationFieldsObject emptyRegistrationFieldsObject = new RegistrationFieldsObject("", "");
+        fillRegistrationFields(emptyRegistrationFieldsObject);
+    }
+
+
+}
