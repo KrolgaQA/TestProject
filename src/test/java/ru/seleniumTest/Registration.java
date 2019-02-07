b1b7d5f7287870bdfa85440d408c730ce0bfba2f
Olga <ol.kras.andr@gmail.com>
2019-02-07 15:31:40 +0300

    refactoring-base-test-hierarchy

--- src/test/java/ru/seleniumTest/Registration.java
+++ src/test/java/ru/seleniumTest/Registration.java
@@ -1,27 +1,8 @@
 package ru.seleniumTest;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class Registration {
-
-    private static WebDriver driver;
-    private String baseUrl = "http://localhost:8080/registration-form.html";
-
-    @BeforeClass
-    public static void setUp() {
-
-        System.setProperty("webdriver.chrome.driver", "F:\\Project\\TestProject\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
-
-    }
+public class Registration extends BaseDriverClass {
 
     @Test
     public void getRegistrationFormWithValidValues(){
@@ -35,33 +16,5 @@ public class Registration {
         fillRegistrationFields(emptyRegistrationFieldsObject);
     }
 
-    private void fillRegistrationFields(RegistrationFieldsObject registrationFieldsObject) {
-        openMainPage();
-        fillRegistrationForm(registrationFieldsObject);
-        submitRegistration();
-    }
 
-    private void submitRegistration() {
-        driver.findElement(By.className("btn-primary")).click();
-    }
-
-    private void fillRegistrationForm(RegistrationFieldsObject registrationFieldsObject) {
-        type("email", registrationFieldsObject.getLogin());
-        type("password", registrationFieldsObject.getPassword());
-    }
-
-    private void type(String locator, String fieldName) {
-        driver.findElement(By.name(locator)).clear();
-        driver.findElement(By.name(locator)).sendKeys(fieldName);
-    }
-
-    private void openMainPage() {
-        driver.get(baseUrl);
-    }
-
-
-    @AfterClass
-    public void shutDown(){
-        driver.close();
-    }
 }
